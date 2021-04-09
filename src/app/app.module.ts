a5c8a41c3605251ef2855781f9c2cdd61eceb24b
Christopher Esparza <christopher52@live.com.mx>
2021-04-08 21:49:57 -0500

    income and expenses

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FormComponent } from './form/form.component';
+import { IngresosComponent } from './ingresos/ingresos.component';
+import { EgresosComponent } from './egresos/egresos.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [				
+    AppComponent,
+      HeaderComponent,
+      FormComponent,
+      IngresosComponent,
+      EgresosComponent
+   ],
   imports: [
     BrowserModule
   ],
